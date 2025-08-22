798060f8112352aa2afa5bc8729557e5268ea161
pulssart <73292964+pulssart@users.noreply.github.com>
2025-08-22 16:58:49 +0200

    Initial Commit

---
+++ Flux/FluxApp.swift
@@ -0,0 +1,17 @@
+//
+//  FluxApp.swift
+//  Flux
+//
+//  Created by Adrien Donot on 22/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct FluxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
